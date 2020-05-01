88748e914f69b044b533b57d4e6f287213bcbb88
Felix Ohnesorge <felix.ohnesorge@8qfx1ai5.de>
2020-05-01 21:44:19 +0200

    implement validate token route (currently broken)
    Co-authored-by: Tino "Nox" Stöckel <itspandorasnox@gmail.com>

--- cmd/jwt-fake-user-flow/main.go
+++ cmd/jwt-fake-user-flow/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -10,6 +12,7 @@ import (
 
 func main() {
 	http.HandleFunc("/jwt", handleJWT)
+	http.HandleFunc("/someresource", handleSomeResource)
 	log.Printf("server stopped: %s", http.ListenAndServe(":8080", nil))
 }
 
@@ -25,6 +28,51 @@ func handleJWT(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "plain/text")
 	w.Write([]byte(token))
+}
+
+func handleSomeResource(w http.ResponseWriter, r *http.Request) {
+
+	headerToken := r.Header.Get("Authorization")
+	splitToken := strings.Split(headerToken, "Bearer")
+	maybeJWT := strings.TrimSpace(splitToken[1])
+
+	token, err := validateToken(maybeJWT)
+	_ = token
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "plain/text")
+	// w.Write([]byte(token))
+}
+
+func validateToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		fmt.Println(token.Method.Alg())
+
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return hmacSampleSecret, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("parsing the jwt didn't worked: %s", err)
+	}
+
+	//this is a type assertion @see https://tour.golang.org/methods/15
+	//type MapClaims map[string]interface{}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// fmt.Println(claims["foo"], claims["nbf"])
+		return claims, nil
+	} else {
+		// fmt.Println(err)
+		return nil, fmt.Errorf("claims not ok or token not valid: %s", err)
+	}
 }
 
 var hmacSampleSecret = []byte("my_secret_key")
